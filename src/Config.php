8a9ac4d597e71d50cdb0d15c0c7b9f50aa07f211
Daniel Kahl <kontakt@daniel-kahl.de>
2016-04-12 22:56:48 +0200

    dev

--- src/Config.php
+++ src/Config.php
@@ -3,6 +3,111 @@
 class Config
 {
 
-  //
+  private $_data = [];
+
+  public function __construct(array $data)
+  {
+    $this->_data = $data;
+  }
+
+  /**
+   * @param string $path
+   * @return bool
+   */
+  public function has($path)
+  {
+    $keys = $this->_splitPath($path);
+    $scope = $this->_getScope($keys);
+
+    return !is_null($scope) && array_key_exists(end($keys), $scope);
+  }
+
+  /**
+   * @param string $path
+   * @param mixed|null $default
+   * @return mixed|null
+   */
+  public function get($path, $default = null)
+  {
+    $keys = $this->_splitPath($path);
+    $scope = $this->_getScope($keys);
+    $last = end($keys);
+
+    return !is_null($scope) && array_key_exists($last, $scope) ? $scope[$last] : $default;
+  }
+
+  /**
+   * @param string $path
+   * @param mixed $value
+   * @param bool $force
+   * @return $this
+   */
+  public function set($path, $value, $force = false)
+  {
+    $keys = $this->_splitPath($path);
+    $scope = &$this->_getScope($keys, $force);
+    $last = end($keys);
+
+    if (!is_null($scope)) {
+      $scope[$last] = $value;
+    }
+
+    return $this;
+  }
+
+  /**
+   * @param string $path
+   * @return $this
+   */
+  public function remove($path)
+  {
+    $keys = $this->_splitPath($path);
+    $scope = &$this->_getScope($keys);
+    $last = end($keys);
+
+    if (!is_null($scope)) {
+      unset($scope[$last]);
+    }
+
+    return $this;
+  }
+
+  /**
+   * @param string $path
+   * @return string[]
+   */
+  private function _splitPath($path)
+  {
+    return explode('.', $path);
+  }
+
+  /**
+   * @param string[] $keys
+   * @param bool $force
+   * @return array|null
+   */
+  private function &_getScope($keys, $force = true)
+  {
+    $scope = &$this->_data;
+
+    if (count($keys) > 1) {
+      foreach ($keys as $i=>$key) {
+        if (count($keys)-1 !== $i) {
+          if (!is_array($scope)) {
+            return null;
+          }
+          if (!array_key_exists($key, $scope)) {
+            if (!$force) {
+              return null;
+            }
+            $scope[$key] = [];
+          }
+          $scope = &$scope[$key];
+        }
+      }
+    }
+
+    return $scope;
+  }
 
 }
--- test/SimpleConfigTest.php
+++ test/SimpleConfigTest.php
@@ -3,9 +3,69 @@
 class SimpleConfigTest extends \PHPUnit_Framework_TestCase
 {
 
-  public function test()
+  /**
+   * @var Config
+   */
+  private $config;
+
+  public function setUp()
   {
-    //
+    $this->config = new Config([
+        "lv1a" => [
+            "lv2a" => [
+                "lv3a" => "foobarA"
+            ]
+        ],
+        "lv1b" => [
+            "lv2b" => "foobarb"
+        ],
+        "lv1c" => [],
+        "foo" => "bar"
+    ]);
+  }
+
+  public function testHas()
+  {
+    $this->assertTrue($this->config->has("foo"));
+    $this->assertTrue($this->config->has("lv1a"));
+    $this->assertTrue($this->config->has("lv1a.lv2a"));
+    $this->assertTrue($this->config->has("lv1a.lv2a.lv3a"));
+    $this->assertFalse($this->config->has("i.am.not.there"));
+  }
+
+  public function testGet()
+  {
+    $this->assertEquals("bar", $this->config->get("foo"));
+    $this->assertEquals(
+        [
+            "lv2a" => [
+                "lv3a" => "foobarA"
+            ]
+        ],
+        $this->config->get("lv1a")
+    );
+    $this->assertEquals(
+        [
+            "lv3a" => "foobarA"
+        ],
+        $this->config->get("lv1a.lv2a")
+    );
+    $this->assertEquals("foobarA", $this->config->get("lv1a.lv2a.lv3a"));
+    $this->assertNull($this->config->get("i.am.not.there"));
+    $this->assertEquals("test", $this->config->get("i.am.not.there", "test"));
+  }
+
+  public function testSet()
+  {
+    $this->assertEquals("test", $this->config->set("test", "test")->get("test"));
+    $this->assertEquals("test", $this->config->set("test2.test", "test", true)->get("test2.test"));
+    $this->assertEquals("test", $this->config->set("lv1a.test", "test")->get("lv1a.test"));
+  }
+
+  public function testRemove()
+  {
+    $this->assertNull($this->config->remove("lv1a.lv12")->get("lv1a.lv12"));
+    $this->assertNull($this->config->remove("foo")->get("foo"));
   }
 
 }
