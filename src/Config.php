ed972a6ee604ca6ae47c44583a49949d1c5bfbaa
Daniel Kahl <kontakt@daniel-kahl.de>
2016-04-12 21:10:30 +0200

    initial structure commit

---
+++ src/Config.php
@@ -0,0 +1,8 @@
+<?php namespace Dtkahl\SimpleConfig;
+
+class Config
+{
+
+  //
+
+}
---
+++ test/SimpleConfigTest.php
@@ -0,0 +1,11 @@
+<?php namespace Dtkahl\SimpleConfig;
+
+class SimpleConfigTest extends \PHPUnit_Framework_TestCase
+{
+
+  public function test()
+  {
+    //
+  }
+
+}
