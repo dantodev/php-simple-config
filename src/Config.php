c80ee3c63300c73c1c368ff2a3cd0ccf1882eeab
daniel <kontakt@daniel-kahl.de>
2016-05-20 16:46:23 +0200

    * add PSR-2 compatibility #1

--- src/Config.php
+++ src/Config.php
@@ -3,111 +3,111 @@
 class Config
 {
 
-  private $_data = [];
+    private $_data = [];
 
-  public function __construct(array $data)
-  {
-    $this->_data = $data;
-  }
-
-  /**
-   * @param string $path
-   * @return bool
-   */
-  public function has($path)
-  {
-    $keys = $this->_splitPath($path);
-    $scope = $this->_getScope($keys);
-
-    return !is_null($scope) && array_key_exists(end($keys), $scope);
-  }
-
-  /**
-   * @param string $path
-   * @param mixed|null $default
-   * @return mixed|null
-   */
-  public function get($path, $default = null)
-  {
-    $keys = $this->_splitPath($path);
-    $scope = $this->_getScope($keys);
-    $last = end($keys);
-
-    return !is_null($scope) && array_key_exists($last, $scope) ? $scope[$last] : $default;
-  }
-
-  /**
-   * @param string $path
-   * @param mixed $value
-   * @param bool $force
-   * @return $this
-   */
-  public function set($path, $value, $force = false)
-  {
-    $keys = $this->_splitPath($path);
-    $scope = &$this->_getScope($keys, $force);
-    $last = end($keys);
-
-    if (!is_null($scope)) {
-      $scope[$last] = $value;
+    public function __construct(array $data)
+    {
+        $this->_data = $data;
     }
 
-    return $this;
-  }
+    /**
+     * @param string $path
+     * @return bool
+     */
+    public function has($path)
+    {
+        $keys = $this->_splitPath($path);
+        $scope = $this->_getScope($keys);
 
-  /**
-   * @param string $path
-   * @return $this
-   */
-  public function remove($path)
-  {
-    $keys = $this->_splitPath($path);
-    $scope = &$this->_getScope($keys);
-    $last = end($keys);
-
-    if (!is_null($scope)) {
-      unset($scope[$last]);
+        return !is_null($scope) && array_key_exists(end($keys), $scope);
     }
 
-    return $this;
-  }
+    /**
+     * @param string $path
+     * @param mixed|null $default
+     * @return mixed|null
+     */
+    public function get($path, $default = null)
+    {
+        $keys = $this->_splitPath($path);
+        $scope = $this->_getScope($keys);
+        $last = end($keys);
 
-  /**
-   * @param string $path
-   * @return string[]
-   */
-  private function _splitPath($path)
-  {
-    return explode('.', $path);
-  }
+        return !is_null($scope) && array_key_exists($last, $scope) ? $scope[$last] : $default;
+    }
 
-  /**
-   * @param string[] $keys
-   * @param bool $force
-   * @return array|null
-   */
-  private function &_getScope($keys, $force = true)
-  {
-    $scope = &$this->_data;
+    /**
+     * @param string $path
+     * @param mixed $value
+     * @param bool $force
+     * @return $this
+     */
+    public function set($path, $value, $force = false)
+    {
+        $keys = $this->_splitPath($path);
+        $scope = &$this->_getScope($keys, $force);
+        $last = end($keys);
 
-    if (count($keys) > 1) {
-      foreach ($keys as $i=>$key) {
-        if (count($keys)-1 !== $i) {
-          if (!is_array($scope)) {
-            return null;
-          }
-          if (!array_key_exists($key, $scope)) {
-            if (!$force) {
-              return null;
-            }
-            $scope[$key] = [];
-          }
-          $scope = &$scope[$key];
+        if (!is_null($scope)) {
+            $scope[$last] = $value;
         }
-      }
+
+        return $this;
     }
 
-    return $scope;
-  }
+    /**
+     * @param string $path
+     * @return $this
+     */
+    public function remove($path)
+    {
+        $keys = $this->_splitPath($path);
+        $scope = &$this->_getScope($keys);
+        $last = end($keys);
+
+        if (!is_null($scope)) {
+            unset($scope[$last]);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param string $path
+     * @return string[]
+     */
+    private function _splitPath($path)
+    {
+        return explode('.', $path);
+    }
+
+    /**
+     * @param string[] $keys
+     * @param bool $force
+     * @return array|null
+     */
+    private function &_getScope($keys, $force = true)
+    {
+        $scope = &$this->_data;
+
+        if (count($keys) > 1) {
+            foreach ($keys as $i => $key) {
+                if (count($keys) - 1 !== $i) {
+                    if (!is_array($scope)) {
+                        return null;
+                    }
+                    if (!array_key_exists($key, $scope)) {
+                        if (!$force) {
+                            return null;
+                        }
+                        $scope[$key] = [];
+                    }
+                    $scope = &$scope[$key];
+                }
+            }
+        }
+
+        return $scope;
+    }
 
 }
